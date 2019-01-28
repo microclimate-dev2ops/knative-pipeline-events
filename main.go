ab7f6a4e914b1e243ee41d0c10e75acfdc6b3be5
dibbles <dibbles@users.noreply.github.com>
2019-01-28 09:14:02 +0000

    manual trigger

--- main.go
+++ main.go
@@ -14,10 +14,85 @@ import (
 	gh "gopkg.in/go-playground/webhooks.v3/github"
 )
 
-func handleWebhook(w http.ResponseWriter, r *http.Request) {
+// Manual subsmission data struct
+type BuildRequest struct {
+	//Example Paylod
+	//{
+	//	"repourl": "https://github.ibm.com/duanes-org/slim-devops-test-overrides-project",
+	//	"commitid": "7d84981c66718ee2dda1af280f915cc2feb9d275",
+	//	"reponame": "slim-devops-test-overrides-project"
+	//
+	//}
+
+	REPOURL  string `json:"repourl"`
+	COMMITID string `json:"commitid"`
+	REPONAME string `json:"reponame"`
+}
+
+func handleManualBuildRequest(w http.ResponseWriter, r *http.Request) {
 
 	// fmt.Printf("headers: %v\n", r.Header)
 	// ToDo: check the CE-X-CE TYPE header to know wht type of gh Payload it will be
+
+	requestData := BuildRequest{}
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		log.Println("OH CACK - GONE WRONG")
+		return
+	}
+
+	log.Println("============================")
+	log.Println(requestData.REPOURL)
+	log.Println(requestData.COMMITID)
+	log.Println(requestData.REPONAME)
+	log.Println("============================")
+
+	varmap := map[string]interface{}{
+		"URL":     requestData.REPOURL,
+		"SHORTID": requestData.COMMITID[0:7],
+		"ID":      requestData.COMMITID,
+		"NAME":    requestData.REPONAME,
+	}
+
+	configString, err := modifyYaml(varmap, "templates/resource.yaml", "edited-resource.yaml")
+	if err != nil {
+		log.Println("WA WA WAAAAAA")
+	}
+
+	log.Println("============================")
+	log.Println(configString)
+	log.Println("============================")
+
+	configString, err = modifyYaml(varmap, "templates/pipeline-run.yaml", "edited-pipeline-run.yaml")
+	if err != nil {
+		log.Println("WA WA WAAAAAA 2")
+	}
+
+	log.Println("============================")
+	log.Println(configString)
+	log.Println("============================")
+
+	output, err := applyYaml("/tmp/edited-resource.yaml")
+	if output != nil {
+		log.Printf("%s", output)
+	}
+	if err != nil {
+		log.Println("UH OH!!")
+		return
+	}
+
+	output, err = applyYaml("/tmp/edited-pipeline-run.yaml")
+	if output != nil {
+		log.Printf("%s", output)
+	}
+	if err != nil {
+		log.Println("UH OH!!")
+		return
+	}
+
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
 	webhookData := gh.PushPayload{}
 	err := json.NewDecoder(r.Body).Decode(&webhookData)
@@ -94,6 +169,7 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		}
 
 		pipeClient. */
+
 }
 
 func modifyYaml(gitAttrs map[string]interface{}, templateToChange, templateOutputFile string) (string, error) {
@@ -142,5 +218,6 @@ func applyYaml(filename string) ([]byte, error) {
 func main() {
 	log.Println("knative-devops-runtime server started")
 	http.HandleFunc("/", handleWebhook)
+	http.HandleFunc("/manual", handleManualBuildRequest)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
